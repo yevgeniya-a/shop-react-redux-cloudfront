66728fe9ea91c604d4f37ed12d558f56e755dab2
Yevgeniya.Anossova <yevgeniya.anossova@wolterskluwer.com>
2021-09-16 22:45:56 +0600

    fix: update import service integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://gtax0177kl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://8repisle0k.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
